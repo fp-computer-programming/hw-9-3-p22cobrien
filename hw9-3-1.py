2fee78dccb965c99d536007605ab6ef2a49810e3
p22cobrien <p22cobrien@fairfieldprep.org>
2022-01-14 09:43:45 -0500

    submit hw9-3-1.py

--- hw9-3-1.py
+++ hw9-3-1.py
@@ -1,17 +1,30 @@
 # Author: CMOB 1/13/2022
 
-from hashlib import new
+#from hashlib import new
+#from tokenize import Name
 
+while True:
+    temp = float(input("What is the temperature?"))
+    conver = str.capitalize(input("Is it Celsius(C) or Fahrenheit(F)?"))
+    try:
+        if conver == "C":
+            if temp == 0:
+                new_temp = 32
+            else:
+                new_temp = temp / 5
+        elif conver == "F":
+            new_temp = temp - 32 / 9
 
-temp = float(input("What is the temperature?"))
-conver = str.capitalize(input("Is it Celsius(C) or Fahrenheit(F)?"))
-try:
-    if conver == "F":
-        new_temp = temp / 5
-    elif conver == "C":
-        new_temp = temp - 32 / 9
-    print(new_temp)
-except ZeroDivisionError:
-    print(-17.18)
-except:
-    print("P")
+    except ValueError:
+        print("Please input an appropriate number.")
+    except NameError:
+        print("Please input either C or F for temperature type.")
+
+    finally:
+        try:
+            print(new_temp)
+        finally:
+            again = str.capitalize(input("Would you like to try again? Y or N"))
+            if again == "N":
+                break
+
